53dc203f66a79a5573a680a3c5191c4dafae2aaa
fellippemendonca <fellippe.mendonca@gmail.com>
2017-10-05 14:17:51 -0300

    Client Stocks Page Updated

--- src/index.js
+++ src/index.js
@@ -9,25 +9,52 @@ class ItemLister extends React.Component {
  		 this.state={ items:[] };
   }
   componentDidMount(){
-    //fetch('http://jsonplaceholder.typicode.com/posts')
-    fetch('/api/stocks')
-    .then(result=>result.json())
-    .then(items=>this.setState({items}))
+    setInterval(()=>{
+      fetch('/api/stocks')
+      .then(result=>result.json())
+      .then(items=>this.setState({items}))
+    }, 1000);
+    
   }
   render() {
   	return (
-    	<ul>
-          {
-            this.state.items.length ?
-          	this.state.items.map(item => <li key={item.id}>{item.name}</li>) 
-            : <li>Loading...</li>
-          }
-      </ul>
+      <table>
+      <thead>
+      <tr>
+        <th><h1>ID</h1></th>
+        <th><h1>Stock Name</h1></th>
+        <th><h1>Current Price</h1></th>
+        <th><h1>Last Update Date / Time</h1></th>
+      </tr>
+    </thead>
+      <tbody>
+        {
+          this.state.items.length ?
+          this.state.items.map(item => 
+          <tr>
+            <td>{item.id}</td><td>{item.name}</td><td>$ {item.currentPrice}</td><td>{item.lastUpdate}</td>
+          </tr>
+          )
+          : <tr>
+            <td>0</td><td>Loading...</td><td>Loading...</td><td>Loading...</td>
+          </tr>
+        }
+        </tbody>
+        </table>
    )
   }
 };
 
 ReactDOM.render(
-  <ItemLister />,
+  <ItemLister/>,
   document.getElementById('root')
 );
+
+
+/*
+<div class="table-title">
+        <h3>Data Table</h3>
+      </div>
+
+      <th class="text-left">{item.name}</th><th class="text-left">{item.currentPrice}</th>
+*/
