c4c135090b99e4719aab27eaac08fe67e4c2d5fe
fellippemendonca <fellippe.mendonca@gmail.com>
2017-10-05 10:21:35 -0300

    Update at index

--- src/index.js
+++ src/index.js
@@ -1,41 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import JsonTable from 'react-json-table';
 import './index.css';
-//import '../app/initializers/index.js';
-import App from './App';
 
 
+class ItemLister extends React.Component {
+	constructor() {
+  	super();
+ 		 this.state={ items:[] };
+  }
+  componentDidMount(){
+    //fetch('http://jsonplaceholder.typicode.com/posts')
+    fetch('http://localhost:3001/api/stocks')
+    .then(result=>result.json())
+    .then(items=>this.setState({items}))
+  }
+  render() {
+  	return (
+    	<ul>
+          {
+            this.state.items.length ?
+          	this.state.items.map(item => <li key={item.id}>{item.name}</li>) 
+            : <li>Loading...</li>
+          }
+      </ul>
+   )
+  }
+};
 
 ReactDOM.render(
-  <App/>,
+  <ItemLister />,
   document.getElementById('root')
 );
-
-/*
-import React, { Component } from 'react';
-import './index.css';
-
-class appData extends Component {
-  state = { stocks: [] }
-    
-  componentDidMount() {
-    fetch('/api/stocks')
-      .then(res => { console.log('state'); return res.json() })
-      .then(stocks => this.setState({ stocks }));
-  }
-
-  render() {
-    return (
-      <div className="appData">
-        <h1>stocks</h1>
-        {this.state.stocks.map(stock =>
-          <div key={stock.id}>{stock.name}</div>
-        )}
-      </div>
-    );
-  }
-}
-
-export default appData;
-*/
